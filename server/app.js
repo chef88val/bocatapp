c665ab50d846ec4b7b70e003ca777fb7c8b7e446
Javier <jsm.multimedia@gmail.com>
2018-07-25 20:48:04 +0200

    bcrypt

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var app = express();
-var routes = require('./routes/api')  
+var routes = require('./routes/api')
+var fs = require('fs');
 module.exports = app;
 
 app.use(bodyParser.urlencoded({
@@ -30,4 +31,25 @@ global.fnPagination = (page) => {
     }
     var itemsPage = 50;
     return itemsPage * page
-} 
+}
+//console.log(this.cryptPassword('everis'))
+var authEmail = JSON.parse(fs.readFileSync('./config.json','utf-8'))
+var nodeoutlook = require('nodejs-nodemailer-outlook')
+
+function sendEmail() {
+    
+    console.log(authEmail.user+authEmail.pass)
+    nodeoutlook.sendEmail({
+        auth: {
+            user: authEmail.user,
+            pass: authEmail.pass
+        },
+
+        from: 'jsegarrm@everis.com',
+        to: 'jsm.multimedia@gmail.com',
+        subject: 'Hey you, awesome!',
+        html: '<b>This is bold text</b>',
+        text: 'This is text version!'
+    });
+}
+sendEmail()
