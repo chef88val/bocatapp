f9314162f8e8444c2bccb85c044ffc2e18abd68b
Javier <jsm.multimedia@gmail.com>
2018-07-25 22:51:12 +0200

    update controllers to MongoDB

--- server/controllers/pedido.js
+++ server/controllers/pedido.js
@@ -2,24 +2,92 @@ var pedido = require('../models/pedido')
 var mongoosePaginate = require('mongoose-paginate');
 var path = require('path');
 var fs = require('fs');
+var moment = require('moment')
+var utils = require('../utils')
 
 
 function getPedido(req, res, next){
-    res.status(200).send({
-        message: "getPedido OK"
-    })
+    if (req.params.id) {
+
+        pedido.find({
+            _id: req.params.id,
+            visible: true,
+            
+        })
+        .populate('items.user')
+        .populate('items.item')
+        .exec( (err, pedido) => {
+            console.log(err, pedido)
+            if (err) return res.status(500).send({
+                message: 'Error en la peticion'
+            })
+            if (!pedido || pedido.length < 1)
+                return res.status(404).send({
+                    message: 'El pedido no existe'
+                })
+            //if (pedido &&  pedido.length > 1)
+
+            return res.status(200).send({
+                pedido
+            })
+
+        })
+
+    } 
 }
 
-function savePedido(req, res, next) { 
-    res.status(200).send({
-        message: "savePedido OK"
+function savePedido(req, res, next) {     
+    pedido = new pedido({
+        title: utils.returnMomentFormat(),
+        items:[],
+        users:[],
+        author:'system',
+        status:'draft',
+        pubDate: moment(),
+        visible: true
+    });
+    try {
+        pedido.save((err, _pedido) => {
+        console.log(err,_pedido)
+        if (err) return res.status(500).send({
+            message: 'Error en la peticion'
+        })
+        if (!_pedido) return res.status(404).send({
+            message: 'No hay Pedidos disponibles'
+        })
+        return res.status(200).send({
+            message: "Pedido created"
+        })
     })
+    } catch (error) {
+        return res.status(200).send({
+            message: error.errors.size.ValidatorError
+        })
+    }
+     
 }
 
 function updatePedido(req, res, next){
-    res.status(200).send({
-        message: "updatePedido OK"
-    })
+    if (req.params.id) {
+        var update = req.body;
+        console.log('1',req.body)
+         pedido.findByIdAndUpdate(req.params.id, 
+            //update, 
+            { $push: { items: update } },
+            (err, _pedido) => {
+             console.log('updatePedido',err)
+             if (err) return res.status(500).send({
+                 message: 'Error en la peticion'
+             })
+             if (!_pedido) return res.status(404).send({
+                 message: 'No hay Pedidos disponibles'
+             })
+             //if (_pedido) 
+             return res.status(200).send({
+                 message: "Pedido updated"
+             })
+         })
+        }
 }
 
 function deletePedido(req, res, next) { 
--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,33 @@
-var user = require('../models/user')
+var User = require('../models/user')
 var mongoosePaginate = require('mongoose-paginate');
 var path = require('path');
 var fs = require('fs');
 
 
 function getUser(req, res, next) {
-    res.status(200).send({
-        message: "getUser OK"
-    })
+    if (req.params.id) { 
+
+        User.findById({
+            _id: req.params.id,
+            visible: true
+        }, (err, user) => {
+            console.log(err, user)
+            if (err) return res.status(500).send({
+                message: 'Error en la peticion'
+            })
+            if (!user || user.length < 1)
+                return res.status(404).send({
+                    message: 'El user no existe'
+                })
+            //if (user &&  user.length > 1)
+
+            return res.status(200).send({
+                user
+            })
+
+        })
+
+    } 
 }
 
 function saveUser(req, res, next) {
@@ -17,9 +37,49 @@ function saveUser(req, res, next) {
 }
 
 function updateUser(req, res, next) {
-    res.status(200).send({
-        message: "updateUser OK"
-    })
+    if (req.params.id) {
+        var update = req.body;
+        console.log('1',req.body)
+         update.visible = true;
+         User.findByIdAndUpdate(req.params.id, update, {
+             new: true
+         }, (err, _user) => {
+             console.log('updateUser',err)
+             if (err) return res.status(500).send({
+                 message: 'Error en la peticion'
+             })
+             if (!_user) return res.status(404).send({
+                 message: 'No hay Users disponibles'
+             })
+             //if (_user) 
+             return res.status(200).send({
+                 message: "User updated"
+             })
+         })
+     } else {
+         console.log('2');
+         User = new User(req.body);
+         try {
+               User.save((err, _user) => {
+             console.log(err,_user)
+             if (err) return res.status(500).send({
+                 message: 'Error en la peticion'
+             })
+             if (!_user) return res.status(404).send({
+                 message: 'No hay Users disponibles'
+             })
+             return res.status(200).send({
+                 message: "User created"
+             })
+         })
+         } catch (error) {
+             return res.status(200).send({
+                 message: error.errors.size.ValidatorError
+             })
+         }
+         
+       
+     }
 }
 
 function deleteUser(req, res, next) {
