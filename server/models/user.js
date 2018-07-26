790785c9cf03a832d1ca73dfe0e0893956986b99
javier <jsm.multimedia@gmail.com>
2018-07-26 14:12:18 +0200

    'migrations'

--- server/models/user.js
+++ server/models/user.js
@@ -8,6 +8,12 @@ var userSchema = Schema({
     email: String,
     money: {type:Number, default:0},
     lastCall: Date,
+    profile:  {type:String,enum: ['SF'], default:'SF'},
+    role: {
+        type: String,
+        enum: ['user', 'admin', 'caller']
+    },
+    notify:  {type:Boolean, default:true},
     visible: {type:Boolean, default:true}
 })
 
