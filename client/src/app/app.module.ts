89126c8bd0d305d52d20c23b36ac389c616c45e7
javier <jsm.multimedia@gmail.com>
2018-07-27 09:39:30 +0200

    init component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PedirComponent } from './components/pedir/pedir.component';
+import { ConsultarComponent } from './components/consultar/consultar.component';
+import { ReservarComponent } from './components/reservar/reservar.component';
+import { NavbarComponent } from './components/extras/navbar/navbar.component';
+import { FooterComponent } from './components/extras/footer/footer.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PedirComponent,
+    ConsultarComponent,
+    ReservarComponent,
+    NavbarComponent,
+    FooterComponent,
+    AdminComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
