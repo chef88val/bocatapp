6d9894b3ad23aa13fe80f0fc0763befad0efa242
javier <jsm.multimedia@gmail.com>
2018-07-27 11:42:55 +0200

    modulesintoapp

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+// Import your library
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { RestangularModule, Restangular } from 'ngx-restangular';
+import {TranslateModule} from '@ngx-translate/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { PedirComponent } from './components/pedir/pedir.component';
 import { ConsultarComponent } from './components/consultar/consultar.component';
@@ -22,9 +26,18 @@ import { ProfileComponent } from './components/profile/profile.component';
     ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot(),
+    RestangularModule.forRoot(RestangularConfigFactory),
+    NgxPermissionsModule.forRoot(),
+    TranslateModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// Function for setting the default restangular configuration
+export function RestangularConfigFactory (RestangularProvider) {
+  RestangularProvider.setBaseUrl('http://api.restngx.local/v1');
+  RestangularProvider.setDefaultHeaders({'Authorization': 'Bearer UDXPx-Xko0w4BRKajozCVy20X11MRZs1'});
+}
