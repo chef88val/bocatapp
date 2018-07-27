1a1598e2482e48a866abd731341f85b1cfcda21d
javier <jsm.multimedia@gmail.com>
2018-07-27 16:43:09 +0200

    detodounpoco

---
+++ client/src/app/user.ts
@@ -0,0 +1,17 @@
+export class User {
+    public name: String;
+    public email: String;
+    public lastCall: Date;
+    public profile: String;
+    public role: String;
+    public money: Number;
+    public notify: Boolean = true;
+    public visible: Boolean = true;
+
+    constructor(name: String, email: String, role: String, profile: String){
+        this.name = name;
+        this.email = email;
+        this.role = role;
+        this.profile = profile;
+    }
+}
